eb344dfb654156f86b04fb102cdc4f251d091a0d
deeptrip <deeptrip@din73003013.corp.capgemini.com>
2018-11-23 14:07:27 +0530

    First Commit

---
+++ Day1-Demo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,12 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome! to GitHub!!");
+		System.out.println("Welcome!! Deepak Tripathi");
+
+	}
+
+}
