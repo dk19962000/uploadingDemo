7015928c67c203b30d1e26794fb5b962e3c35456
deeptrip <deeptrip@din73003013.corp.capgemini.com>
2018-11-23 14:23:03 +0530

    Second Commit

--- Day1-Demo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-Demo/src/main/java/org/cap/demo/MainClass.java
@@ -1,12 +1,17 @@
 package org.cap.demo;
 
 public class MainClass {
-
+	
+	public static String sayHello() {
+		
+		return "Deepak";
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome! to GitHub!!");
 		System.out.println("Welcome!! Deepak Tripathi");
-
+		System.out.println(sayHello());
 	}
 
 }
